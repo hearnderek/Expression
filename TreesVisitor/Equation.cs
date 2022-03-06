e00018bf40140aacb8cc3a78a17ffa0b3225d75b
hearnderek <hearnderek@yahoo.com>
2022-03-06 14:14:00 +0900

    initial with variable expression evaluation for integers

---
+++ TreesVisitor/Equation.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionTree
+{
+
+    public abstract class Expression
+    {
+        public Expression parent;
+        public List<Expression> expressions = new List<Expression>();
+        public abstract Value Evaluate(Dictionary<string, int> variableDefinitions = null);
+
+        public Expression(IEnumerable<Expression> expressions = null, Expression parent = null)
+        {
+            this.parent = parent;
+            if (expressions != null)
+            {
+                this.expressions = expressions.ToList();
+
+                foreach(var expression in expressions)
+                {
+                    expression.parent = this;
+                }
+            }
+            else
+            {
+                this.expressions = new List<Expression>();
+            }
+        }
+    }
+
+    public abstract class BinaryOperation : Expression
+    {
+        public Expression left;
+        public Expression right;
+
+        public BinaryOperation(Expression left, Expression right, Expression parent = null) : 
+            base(new[] { left, right }, parent)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public class Add : BinaryOperation
+        {
+            public Add(Expression left, Expression right, Expression parent = null) : base(left, right, parent)
+            {
+            }
+
+            public override Value Evaluate(Dictionary<string, int> variableDefinitions = null)
+            {
+                return new Value(left.Evaluate(variableDefinitions).value + right.Evaluate(variableDefinitions).value);
+            }
+        }
+
+        public class Mul : BinaryOperation
+        {
+            public Mul(Expression left, Expression right, Expression parent = null) : base(left, right, parent)
+            {
+            }
+
+            public override Value Evaluate(Dictionary<string, int> variableDefinitions = null)
+            {
+                return new Value(left.Evaluate(variableDefinitions).value * right.Evaluate(variableDefinitions).value);
+            }
+        }
+
+        public class Sub : BinaryOperation
+        {
+            public Sub(Expression left, Expression right, Expression parent = null) : base(left, right, parent)
+            {
+            }
+
+            public override Value Evaluate(Dictionary<string, int> variableDefinitions = null)
+            {
+                return new Value(left.Evaluate(variableDefinitions).value - right.Evaluate(variableDefinitions).value);
+            }
+        }
+
+        public class Div : BinaryOperation
+        {
+            public Div(Expression left, Expression right, Expression parent = null) : base(left, right, parent)
+            {
+            }
+
+            public override Value Evaluate(Dictionary<string, int> variableDefinitions = null)
+            {
+                return new Value(left.Evaluate(variableDefinitions).value - right.Evaluate(variableDefinitions).value);
+            }
+        }
+
+        public class Exp : BinaryOperation
+        {
+            public Exp(Expression left, Expression right, Expression parent = null) : base(left, right, parent)
+            {
+            }
+
+            public override Value Evaluate(Dictionary<string, int> variableDefinitions = null)
+            {
+                return new Value(left.Evaluate(variableDefinitions).value ^ right.Evaluate(variableDefinitions).value);
+            }
+        }
+    }
+
+    public class Value : Expression
+    {
+        public int value;
+        public Value(int value, Expression parent = null)
+            : base(new Expression[] { }, parent)
+        {
+            this.value = value;
+        }
+
+        public override Value Evaluate(Dictionary<string, int> variableDefinitions = null)
+        {
+            return this;
+        }
+    }
+
+    public class Variable : Expression
+    {
+        public string identifier;
+
+        public Variable(string identifier, Expression parent = null) :
+            base(new Expression[0], parent)
+        {
+            this.identifier = identifier;
+        }
+
+
+        public override Value Evaluate(Dictionary<string, int> variableDefinitions = null)
+        {
+            if(variableDefinitions?.ContainsKey(identifier) ?? false)
+            {
+                return new Value(variableDefinitions[identifier]);
+            }
+
+            throw new UndefinedVariableException(this);
+        }
+    }
+
+    public class UndefinedVariableException : Exception
+    {
+        public UndefinedVariableException(Variable variable) : base($"{variable.identifier} was not defined")
+        {
+
+        }
+    }
+}
---
+++ TreesVisitor/Program.cs
@@ -0,0 +1,87 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ExpressionTree;
+
+Console.WriteLine("Hello, World!");
+
+//var calc = new CalculationVisitor();
+//var plus = new AddBinaryNumericNode(new NumericNode(5), new NumericNode(10));
+//plus.Accept(calc);
+//Console.WriteLine("Expecting 15, Got " + calc.result);
+
+//calc = new CalculationVisitor();
+//var mul = new MulBinaryNumericNode(new NumericNode(5), new NumericNode(10));
+//mul.Accept(calc);
+//Console.WriteLine("Expecting 50, Got " + calc.result);
+
+
+/*
+ * 5 + 6 + 7
+ * 
+ * +
+ * 5 +
+ *   6 7
+ */
+
+//calc = new CalculationVisitor();
+
+// 5 + 10
+var expressionTree = 
+    new BinaryOperation.Add(
+        new Value(5),
+        new Value(10));
+Console.WriteLine(expressionTree.Evaluate().value);
+
+
+// 3 + 5 + 7
+expressionTree =
+    new BinaryOperation.Add(
+        new Value(3),
+        new BinaryOperation.Add(
+            new Value(5),
+            new Value(7)));
+Console.WriteLine(expressionTree.Evaluate().value);
+
+
+// 3 + 5 + 7 + 11
+expressionTree =
+    new BinaryOperation.Add(
+        new Value(3),
+        new BinaryOperation.Add(
+            new Value(5),
+            new BinaryOperation.Add(
+                new Value(7),
+                new Value(11))));
+Console.WriteLine(expressionTree.Evaluate().value);
+
+
+// 3 + 5 * 7 + 11
+expressionTree =
+    new BinaryOperation.Add(
+        new Value(3),
+        new BinaryOperation.Add(
+            new BinaryOperation.Mul(
+                new Value(5),
+                new Value(7)
+            ),
+            new Value(11)));
+
+
+// 3 + 5 * x + 11
+expressionTree =
+    new BinaryOperation.Add(
+        new Value(3),
+        new BinaryOperation.Add(
+            new BinaryOperation.Mul(
+                new Value(5),
+                new Variable("x")
+            ),
+            new Value(11)));
+Dictionary<string, int> variableDefinitions = new Dictionary<string, int>() { { "x", 5 } };
+Console.WriteLine(expressionTree.Evaluate( variableDefinitions ).value);
+
+variableDefinitions = new Dictionary<string, int>() { { "x", 10 } };
+Console.WriteLine(expressionTree.Evaluate(variableDefinitions).value);
+
+variableDefinitions = new Dictionary<string, int>() { { "x", 20 } };
+Console.WriteLine(expressionTree.Evaluate(variableDefinitions).value);
